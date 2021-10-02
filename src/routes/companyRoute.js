93cc2f73f0526491c334114e439c02c29605a1fa
Parthudi <65971741+Parthudi@users.noreply.github.com>
2021-10-02 17:34:46 +0530

    adding backend

--- src/routes/companyRoute.js
+++ src/routes/companyRoute.js
@@ -5,17 +5,7 @@ const {Auth, isAuth, isAdmin } = require('../middleware/auth')
 
 //Routes
 route.post('/company/signup' , signupCompany)
-// route.post('/user/login', loginUser)
-// route.param('userid', findUserId)
-
-// route.post('/user/logout/:userid',Auth, isAuth, logoutUser)
 route.get('/companies',Auth,isAdmin, readAllCompanies)
-// route.get("/users/auth" , Auth, autnenticationMessage)
-// route.patch('/user/update/:userid',Auth, isAuth, update )
 
-
-// route.get('/secret/:userid',Auth, isAuth,isAdmin, (req, res) => {
-//     res.json('done')
-// })
 
 module.exports = route
